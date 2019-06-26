65e5db16343764f31915d5c341a4fdecee9e96c0
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2019-06-25 22:21:51 -0700

    added Nav link to style active links on main menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,35 +1,8 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import '../css/responsive.css'
 
-
-
 class Navigation extends Component {
-    constructor(props){
-        super(props);
-        
-        // this.setHomeActive = this.setHomeActive.bind(this);
-        this.setAboutActive = this.setAboutActive.bind(this);
-        // this.setPortfolioActive = this.setPortfolioActive.bind(this);
-        // this.setContactActive = this.setContactActive.bind(this);
-
-        this.state = {
-            homeLinkColor: '#4CAE51',
-            aboutLinkColor: '#bfbfbf',
-            portfolioLinkColor: '#bfbfbf',
-            contactLinkColor: '#bfbfbf'
-        }
-    }
-
-    setAboutActive(event) {
-        this.setState({
-            homeLinkColor: '#bfbfbf',
-            aboutLinkColor: '#4CAE51'
-        })
-
-        document.getElementById('about-li').style.color = '#4CAE51'
-    }
-
     render() {
         return (
             <header className="theme row">
@@ -38,10 +11,10 @@ class Navigation extends Component {
                         <h1 className="w3-wide">dwyan alford</h1>
                     </div>
                     <ul>
-                        <li><Link id="home-li"  style={{color: this.state.homeLinkColor}}  to='/' >Home</Link></li>
-                        <li><Link id="about-li" onClick={this.setAboutActive} to='/about'>About</Link></li>
-                        <li><Link id="portfolio-li" style={{color: this.state.portfolioLinkColor}} to='/portfolio' >Portfolio</Link></li>
-                        <li><Link id="contact-li" style={{color: this.state.contactLinkColor}} to='/contact' >Contact</Link></li>
+                        <li><NavLink to='/' exact activeClassName="active" >Home</NavLink></li>
+                        <li><NavLink to='/about' activeClassName="active" >About</NavLink></li>
+                        <li><NavLink to='/portfolio' activeClassName="active" >Portfolio</NavLink></li>
+                        <li><NavLink to='/contact' activeClassName="active" >Contact</NavLink></li>
                     </ul>
                 </div>
             </header>
