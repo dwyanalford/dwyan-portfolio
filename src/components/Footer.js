76ed65061eb0885cc0db8a831c4fcada6bf5e4c2
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2019-07-09 04:06:31 -0700

    added social icons to footer using font awesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,12 @@ const Footer = () => {
     return(
         <footer className="row theme">
           <div className="col-12">
-          <p>&copy; { new Date().getFullYear() }
+          <p id="footer-copytext">&copy; { new Date().getFullYear() }
           &nbsp;- Site Design and Development by Dwyan Alford.
           </p>
+          <p id="footer-social-icons">
+            <i class="fab fa-linkedin fa-3x colorTheme"></i>
+            <i class="fab fa-github-square fa-3x colorTheme"></i></p>
           </div>
         </footer>
     )
