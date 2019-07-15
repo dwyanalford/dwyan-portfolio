c85ad624acfda9b0bb69d5376eb0d104e375ef9a
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2019-07-14 20:03:56 -0700

    turned Hero image into slideshow of images on home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,43 @@
-import React from "react"
+import React, { Component } from "react"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import '../css/hero.css'
+import slide1 from '../images/slideshows/slideshow-hero-1.jpg'
+import slide2 from '../images/slideshows/slideshow-hero-2.jpg'
+import slide3 from '../images/slideshows/slideshow-hero-3.jpg'
 
-const Hero = () => {
-    return(
-            <div className="content-wrap">
+class Hero extends Component {
+    render() {
+        const settings = {
+            dots: false,
+            infinite: true,
+            fade: true,
+            speed: 1000,
+            slidestoShow: 1,
+            slidestoScroll: 1,
+            autoplay: true,
+            cssEase: "linear"
+        }
+        return(
+            <div>
+                <Slider {...settings}>
+                <div className="hero-slide">
+                    <img src={slide1} alt="slideshow gallery showing Dwyan Alford"/>
+                </div>
+    
+                <div className="hero-slide">
+                    <img src={slide2} alt="slideshow gallery showing Dwyan Alford"/>
+                </div>
+            
+                <div className="hero-slide">
+                    <img src={slide3} alt="slideshow gallery showing Dwyan Alford"/>
+                </div>
+                </Slider>
             </div>
-    )
-}
+        )
+    }
+  }
 
 export default Hero
+
