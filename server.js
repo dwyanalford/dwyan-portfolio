9f7dfb57d2d8b418ded2424890f7e0b2fd4677d9
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2019-09-19 16:34:18 -0700

    renamed index to server.js additions to server.js

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
-require('./src/database')
-const Contact = require('./src/models/user')
+require('./database')
+const Contact = require('./models/user')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
 app.use(cors())
+
+// send static file requests to the location you choose on the client
+// this directs to build folder than create-eact-app generates 
+// on production build
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 // to retrieve all my contacts in the mongoDB database
 app.get('/contacts', async (req, res) => {
@@ -43,5 +49,12 @@ app.delete('/contacts/:id', async (req, res) => {
         res.status(500).send(error)
     }
 })
+
+// A 'catch-all' to send the main index.html file back to the client if it didn't 
+// receive a request it recognized otherwise - this will break App on two servers
+// so fix this by adding a proxy in the package json.
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
