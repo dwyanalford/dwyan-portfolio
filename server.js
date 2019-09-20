369fa465822170ac3845a0fa6aa76bc66e331600
Dwyan Alford <dwyanalford@Dwyans-MacBook-Pro.local>
2019-09-19 18:15:58 -0700

    added ability for express to serve up static file in react build folder for production

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors())
 // send static file requests to the location you choose on the client
 // this directs to build folder than create-eact-app generates 
 // on production build
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "./client/build")))
 
 // to retrieve all my contacts in the mongoDB database
 app.get('/contacts', async (req, res) => {
@@ -54,7 +54,7 @@ app.delete('/contacts/:id', async (req, res) => {
 // receive a request it recognized otherwise - this will break App on two servers
 // so fix this by adding a proxy in the package json.
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+    res.sendFile(path.join(__dirname, + './client/build/index.html'))
+})
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
